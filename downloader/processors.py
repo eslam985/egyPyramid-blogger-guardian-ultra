2dcb3583fc1caec175d4a29882848318a6abcbf4
eslam985 <ee17172@gmail.com>
2026-03-04 14:29:40 +0200

    حل مشكلة نشر الحلقة كا صفحة مستقله

--- downloader/processors.py
+++ downloader/processors.py
@@ -473,6 +473,8 @@ def upload_to_vk_local(title, file_path):
         return None
 
 
+
+
 async def upload_to_voe_api(file_path, identifier):
     try:
         async with httpx.AsyncClient(timeout=30.0) as client:  # أضف هذا السطر هنا
