6ce156239269414a3f62ba3aad2ea86041d0a075
eslam985 <ee17172@gmail.com>
2026-03-06 06:32:41 +0200

    حل مشكلة Internal Server Error

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:7860/api', // رابط مباشر
+    // استخدم مساراً نسبياً فقط، والمتصفح سيكمله تلقائياً
+    baseURL: '/api', 
     auth: {
         username: 'ee17172@gmail.com',
         password: 'Eslamsayed@98'
