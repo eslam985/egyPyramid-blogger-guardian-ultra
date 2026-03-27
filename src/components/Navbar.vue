5c85c6c67a7d8953f1e0b4e69de410aafcafa446
eslam985 <ee17172@gmail.com>
2026-03-27 06:27:40 +0200

    تم تعديل مجلد الدانلود ليدعم ملئ بينات الجداول والاعمجه الجديده  في ساب بيز

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,14 @@
 <template>
 
-  <nav class="  grid  grid-cols-1 md:grid-cols-3 gap-10 p-4 shadow-md  z-50 sticky top-px my-card">
+  <nav class=" max-w-500 mx-auto grid  grid-cols-1 md:grid-cols-2 gap-4 p-4 shadow-md  z-50 sticky top-px my-card">
 
-    <div
-      class="font-bold max-w-50 text-3xl text-yellow-500 tracking-[-0.5px] whitespace-nowrap px-4 py-1 rounded-lg shadow-md bg-[linear-gradient(135deg,#8a8000_0,#000000_80%)]">
-      EGY PYRMID
-    </div>
 
-    <div class="flex flex-[0_1_500px] mx-6">
-      <form @submit.prevent="searchMedia" class="flex flex-row-reverse w-full">
 
-        <button type="submit"
-          class="px-5 py-2.5 bg-primary text-white border border-primary rounded-l-lg hover:bg-primary-dark transition-colors flex items-center justify-center">
-          <i class="fa fa-search"></i>
-        </button>
 
-        <input v-model="searchQuery" type="text" placeholder="ابحث..."
-          class="w-full px-4 py-2.5 border border-gray-300 dark:border-gray-600 rounded-r-lg bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 transition-all focus:outline-none focus:border-primary focus:ring-4 focus:ring-blue-500/10">
 
-      </form>
-    </div>
-
-    <div class=" flex gap-2">
-      <router-link to="/database" class="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-800 transition flex items-center gap-2">
+    <div class=" flex gap-4">
+      <router-link to="/database"
+        class="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-800 transition flex items-center gap-2">
         <i class="fa fa-database"></i> قاعدة البيانات
       </router-link>
 
@@ -40,6 +26,27 @@
         تسجيل خروج
       </button>
     </div>
+
+    <div class="flex flex-[0_1_500px] gap-4">
+      <form @submit.prevent="searchMedia" class="flex flex-row-reverse w-full">
+
+        <button type="submit"
+          class="px-5 py-2.5 bg-primary text-white border border-primary rounded-l-lg hover:bg-primary-dark transition-colors flex items-center justify-center">
+          <i class="fa fa-search"></i>
+        </button>
+
+        <input v-model="searchQuery" type="text" placeholder="ابحث..."
+          class="w-full px-4 py-2.5 border border-gray-300 dark:border-gray-600 rounded-r-lg bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 transition-all focus:outline-none focus:border-primary focus:ring-4 focus:ring-blue-500/10">
+
+      </form>
+      <div
+        class="font-bold max-w-50 text-3xl text-yellow-500 tracking-[-0.5px] whitespace-nowrap px-4 py-1 rounded-lg shadow-md bg-[linear-gradient(135deg,#8a8000_0,#000000_80%)]">
+
+        <a href="/"> EGY PYRMID</a>
+
+      </div>
+    </div>
+
   </nav>
 
 </template>
